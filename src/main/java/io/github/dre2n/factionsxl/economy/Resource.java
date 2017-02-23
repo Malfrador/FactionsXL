8a307518f8690195afa246ba256287f0ba26ceb4
Daniel Saukel <sa.da@unitybox.de>
2017-02-24 00:56:00 +0100

    Translatable resource names

--- src/main/java/io/github/dre2n/factionsxl/economy/Resource.java
+++ src/main/java/io/github/dre2n/factionsxl/economy/Resource.java
@@ -28,82 +28,83 @@ public enum Resource {
 
     // MINES
     // Resource
-    COAL(new ItemStack(Material.COAL), 20),
-    SULPHUR(new ItemStack(Material.SULPHUR), 25),
-    GOLD(new ItemStack(Material.GOLD_INGOT), 800),
-    IRON(new ItemStack(Material.IRON_INGOT), 75),
-    DIAMOND(new ItemStack(Material.DIAMOND), 1000),
-    EMERALD(new ItemStack(Material.EMERALD), 750),
-    LAPIS_LAZULI(new ItemStack(Material.INK_SACK, 1, (short) 4), 400),
-    QUARTZ(new ItemStack(Material.QUARTZ), 50),
-    REDSTONE(new ItemStack(Material.REDSTONE), 40),
+    COAL(FMessage.RESOURCE_COAL, new ItemStack(Material.COAL), 20),
+    SULPHUR(FMessage.RESOURCE_SULPHUR, new ItemStack(Material.SULPHUR), 25),
+    GOLD(FMessage.RESOURCE_GOLD, new ItemStack(Material.GOLD_INGOT), 800),
+    IRON(FMessage.RESOURCE_IRON, new ItemStack(Material.IRON_INGOT), 75),
+    DIAMOND(FMessage.RESOURCE_DIAMOND, new ItemStack(Material.DIAMOND), 1000),
+    EMERALD(FMessage.RESOURCE_EMERALD, new ItemStack(Material.EMERALD), 750),
+    LAPIS_LAZULI(FMessage.RESOURCE_LAPIS_LAZULI, new ItemStack(Material.INK_SACK, 1, (short) 4), 400),
+    QUARTZ(FMessage.RESOURCE_QUARTZ, new ItemStack(Material.QUARTZ), 50),
+    REDSTONE(FMessage.RESOURCE_REDSTONE, new ItemStack(Material.REDSTONE), 40),
     // Stones
-    ANDESITE(new ItemStack(Material.STONE, 1, (short) 5), 55),
-    DIORITE(new ItemStack(Material.STONE, 1, (short) 3), 110),
-    GRANITE(new ItemStack(Material.STONE, 1, (short) 1), 160),
-    GRAVEL(new ItemStack(Material.GRAVEL), 2.5),
-    OBSIDIAN(new ItemStack(Material.OBSIDIAN), 100),
-    STONE(new ItemStack(Material.STONE), 5), // Cobblestone, Stone, Smooth bricks
+    ANDESITE(FMessage.RESOURCE_ANDESITE, new ItemStack(Material.STONE, 1, (short) 5), 55),
+    DIORITE(FMessage.RESOURCE_DIORITE, new ItemStack(Material.STONE, 1, (short) 3), 110),
+    GRANITE(FMessage.RESOURCE_GRANITE, new ItemStack(Material.STONE, 1, (short) 1), 160),
+    GRAVEL(FMessage.RESOURCE_GRAVEL, new ItemStack(Material.GRAVEL), 2.5),
+    OBSIDIAN(FMessage.RESOURCE_OBSIDIAN, new ItemStack(Material.OBSIDIAN), 100),
+    STONE(FMessage.RESOURCE_STONE, new ItemStack(Material.STONE), 5), // Cobblestone, Stone, Smooth bricks
     // FARMING
     // Animals
-    CHICKEN(new ItemStack(Material.EGG), 15), // EGGS, CHICKEN
-    COW(new ItemStack(Material.MILK_BUCKET), 25), // LEATHER, BEEF, MILK
-    HORSE(new ItemStack(Material.SADDLE), 100),
-    PIG(new ItemStack(Material.GRILLED_PORK), 20), // PORK
-    RABBIT(new ItemStack(Material.RABBIT_FOOT), 10), // RABBIT, HIDE
-    SHEEP(new ItemStack(Material.WOOL), 20), // MUTTON, WOOL
+    CHICKEN(FMessage.RESOURCE_CHICKEN, new ItemStack(Material.EGG), 15), // EGGS, CHICKEN
+    COW(FMessage.RESOURCE_COW, new ItemStack(Material.MILK_BUCKET), 25), // LEATHER, BEEF, MILK
+    HORSE(FMessage.RESOURCE_HORSE, new ItemStack(Material.SADDLE), 100),
+    PIG(FMessage.RESOURCE_PIG, new ItemStack(Material.GRILLED_PORK), 20), // PORK
+    RABBIT(FMessage.RESOURCE_RABBIT, new ItemStack(Material.RABBIT_FOOT), 10), // RABBIT, HIDE
+    SHEEP(FMessage.RESOURCE_SHEEP, new ItemStack(Material.WOOL), 20), // MUTTON, WOOL
     // Plants
-    APPLE(new ItemStack(Material.APPLE), 4.5),
-    BEETROOT(new ItemStack(Material.BEETROOT), 3.75),
-    CARROT(new ItemStack(Material.CARROT_ITEM), 2.25),
-    CHORUS(new ItemStack(Material.CHORUS_FRUIT), 120),
-    COCOA(new ItemStack(Material.COCOA), 50),
-    MELON(new ItemStack(Material.MELON), 4),
-    POTATO(new ItemStack(Material.POTATO_ITEM), 2),
-    PUMPKIN(new ItemStack(Material.JACK_O_LANTERN), 8),
-    SUGAR(new ItemStack(Material.SUGAR_CANE), 15),
-    WHEAT(new ItemStack(Material.WHEAT), 0.5),
+    APPLE(FMessage.RESOURCE_APPLE, new ItemStack(Material.APPLE), 4.5),
+    BEETROOT(FMessage.RESOURCE_BEETROOT, new ItemStack(Material.BEETROOT), 3.75),
+    CARROT(FMessage.RESOURCE_CARROT, new ItemStack(Material.CARROT_ITEM), 2.25),
+    CHORUS(FMessage.RESOURCE_CHORUS, new ItemStack(Material.CHORUS_FRUIT), 120),
+    COCOA(FMessage.RESOURCE_COCOA, new ItemStack(Material.COCOA), 50),
+    MELON(FMessage.RESOURCE_MELON, new ItemStack(Material.MELON), 4),
+    POTATO(FMessage.RESOURCE_POTATO, new ItemStack(Material.POTATO_ITEM), 2),
+    PUMPKIN(FMessage.RESOURCE_PUMPKIN, new ItemStack(Material.JACK_O_LANTERN), 8),
+    SUGAR(FMessage.RESOURCE_SUGAR, new ItemStack(Material.SUGAR_CANE), 15),
+    WHEAT(FMessage.RESOURCE_WHEAT, new ItemStack(Material.WHEAT), 0.5),
     // WOODS
-    ACACIA(new ItemStack(Material.LOG_2), 12.5),
-    BIRCH(new ItemStack(Material.LOG, 1, (short) 2), 5),
-    DARK_OAK(new ItemStack(Material.LOG_2, 1, (short) 1), 15),
-    JUNGLE(new ItemStack(Material.LOG, 1, (short) 3), 10),
-    OAK(new ItemStack(Material.LOG), 3),
-    SPRUCE(new ItemStack(Material.LOG, 1, (short) 1), 4),
-    PAPER(new ItemStack(Material.PAPER), 1.5),
-    MUSHROOMS(new ItemStack(Material.BROWN_MUSHROOM), 5),
+    ACACIA(FMessage.RESOURCE_ACACIA, new ItemStack(Material.LOG_2), 12.5),
+    BIRCH(FMessage.RESOURCE_BIRCH, new ItemStack(Material.LOG, 1, (short) 2), 5),
+    DARK_OAK(FMessage.RESOURCE_DARK_OAK, new ItemStack(Material.LOG_2, 1, (short) 1), 15),
+    JUNGLE(FMessage.RESOURCE_JUNGLE, new ItemStack(Material.LOG, 1, (short) 3), 10),
+    OAK(FMessage.RESOURCE_OAK, new ItemStack(Material.LOG), 3),
+    SPRUCE(FMessage.RESOURCE_SPRUCE, new ItemStack(Material.LOG, 1, (short) 1), 4),
+    PAPER(FMessage.RESOURCE_PAPER, new ItemStack(Material.PAPER), 1.5),
+    MUSHROOMS(FMessage.RESOURCE_MUSHROOMS, new ItemStack(Material.BROWN_MUSHROOM), 5),
     // SEA
     // Sea animals
-    CODFISH(new ItemStack(Material.RAW_FISH), 2.5),
-    CLOWNFISH(new ItemStack(Material.RAW_FISH, 1, (short) 2), 50),
-    PUFFERFISH(new ItemStack(Material.RAW_FISH, 1, (short) 3), 75),
-    SALMON(new ItemStack(Material.RAW_FISH, 1, (short) 1), 10),
-    INK(new ItemStack(Material.INK_SACK), 17.5),
-    SALT(new ItemStack(Material.SUGAR), 40),
-    WATER(ItemUtil.GUI_WATER_BOTTLE, 0.1),
+    CODFISH(FMessage.RESOURCE_CODFISH, new ItemStack(Material.RAW_FISH), 2.5),
+    CLOWNFISH(FMessage.RESOURCE_CLOWNFISH, new ItemStack(Material.RAW_FISH, 1, (short) 2), 50),
+    PUFFERFISH(FMessage.RESOURCE_PUFFERFISH, new ItemStack(Material.RAW_FISH, 1, (short) 3), 75),
+    SALMON(FMessage.RESOURCE_SALMON, new ItemStack(Material.RAW_FISH, 1, (short) 1), 10),
+    INK(FMessage.RESOURCE_INK, new ItemStack(Material.INK_SACK), 17.5),
+    SALT(FMessage.RESOURCE_SALT, new ItemStack(Material.SUGAR), 40),
+    WATER(FMessage.RESOURCE_WATER, ItemUtil.GUI_WATER_BOTTLE, 0.1),
     // Seabed
-    CLAY(new ItemStack(Material.CLAY_BRICK), 15),
-    PRISMARINE(new ItemStack(Material.PRISMARINE_SHARD), 150),
+    CLAY(FMessage.RESOURCE_CLAY, new ItemStack(Material.CLAY_BRICK), 15),
+    PRISMARINE(FMessage.RESOURCE_PRISMARINE, new ItemStack(Material.PRISMARINE_SHARD), 150),
     // MAGIC
-    DRAGON_BREATH(new ItemStack(Material.DRAGONS_BREATH), 2500),
-    EXPERIENCE((new ItemStack(Material.EXP_BOTTLE)), 50),
-    NETHER_WART(new ItemStack(Material.NETHER_WARTS), 10),
-    PURPUR(new ItemStack(Material.PURPUR_BLOCK), 60),
+    DRAGON_BREATH(FMessage.RESOURCE_DRAGON_BREATH, new ItemStack(Material.DRAGONS_BREATH), 2500),
+    EXPERIENCE(FMessage.RESOURCE_EXPERIENCE, new ItemStack(Material.EXP_BOTTLE), 50),
+    NETHER_WART(FMessage.RESOURCE_NETHER_WART, new ItemStack(Material.NETHER_WARTS), 10),
+    PURPUR(FMessage.RESOURCE_PURPUR, new ItemStack(Material.PURPUR_BLOCK), 60),
     // DESERT
-    CACTUS(new ItemStack(Material.CACTUS), 8),
-    GLASS(new ItemStack(Material.GLASS), 12),
-    RED_SAND_STONE(new ItemStack(Material.RED_SANDSTONE), 3),
-    YELLOW_SAND_STONE(new ItemStack(Material.SANDSTONE), 2),
+    CACTUS(FMessage.RESOURCE_CACTUS, new ItemStack(Material.CACTUS), 8),
+    GLASS(FMessage.RESOURCE_GLASS, new ItemStack(Material.GLASS), 12),
+    RED_SANDSTONE(FMessage.RESOURCE_RED_SANDSTONE, new ItemStack(Material.RED_SANDSTONE), 3),
+    YELLOW_SANDSTONE(FMessage.RESOURCE_YELLOW_SANDSTONE, new ItemStack(Material.SANDSTONE), 2),
     // CITY
-    CRAFT(new ItemStack(Material.WORKBENCH), 100),
-    MANPOWER(new ItemStack(Material.SKULL_ITEM, 1, (short) 3), 4),
-    TAXES(new ItemStack(Material.GOLD_NUGGET), 1);
+    CRAFT(FMessage.RESOURCE_CRAFT, new ItemStack(Material.WORKBENCH), 100),
+    MANPOWER(FMessage.RESOURCE_MANPOWER, new ItemStack(Material.SKULL_ITEM, 1, (short) 3), 4),
+    TAXES(FMessage.RESOURCE_TAXES, new ItemStack(Material.GOLD_NUGGET), 1);
 
     private ItemStack icon;
     private double value;
     private FMessage name;
 
-    Resource(ItemStack icon, double value) {
+    Resource(FMessage name, ItemStack icon, double value) {
+        this.name = name;
         this.icon = icon;
         this.value = value;
     }
@@ -129,7 +130,7 @@ public enum Resource {
      * the name of the resource
      */
     public String getName() {
-        return toString();//name.getMessage();
+        return name.getMessage();
     }
 
     /**
