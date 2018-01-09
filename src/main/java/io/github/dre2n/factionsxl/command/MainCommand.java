ef523d5de634a660e2037e0d94dacad771c0a38f
Daniel Saukel <sa.da@unitybox.de>
2018-01-09 22:02:54 +0100

    Bump year to 2018

--- src/main/java/io/github/dre2n/factionsxl/command/MainCommand.java
+++ src/main/java/io/github/dre2n/factionsxl/command/MainCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Daniel Saukel
+ * Copyright (c) 2017-2018 Daniel Saukel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,7 +47,7 @@ public class MainCommand extends FCommand {
         MessageUtil.sendCenteredMessage(sender, "&4" + F[4] + "&f" + X[4] + L[4]);
         MessageUtil.sendCenteredMessage(sender, "&b&l######## " + FMessage.CMD_MAIN_WELCOME.getMessage() + " &av" + plugin.getDescription().getVersion() + " &b&l########");
         MessageUtil.sendCenteredMessage(sender, FMessage.CMD_MAIN_HELP.getMessage());
-        MessageUtil.sendCenteredMessage(sender, "&7\u00a92017 Daniel Saukel; licensed under GPLv3.");
+        MessageUtil.sendCenteredMessage(sender, "&7\u00a92017-2018 Daniel Saukel; licensed under GPLv3.");
     }
 
 }
