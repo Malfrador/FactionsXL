c83148f6d269806867fa2d2649d312a38450c002
Daniel Saukel <sa.da@unitybox.de>
2018-12-18 21:42:30 +0100

    Old banner data fix

--- src/main/java/de/erethon/factionsxl/faction/Faction.java
+++ src/main/java/de/erethon/factionsxl/faction/Faction.java
@@ -1326,7 +1326,7 @@ public class Faction extends LegalEntity {
         desc = config.getString("desc");
         anthem = config.getString("anthem");
         banner = (BannerMeta) config.get("banner");
-        bannerType = Material.getMaterial(config.getString("bannerType"));
+        bannerType = Material.getMaterial(config.getString("bannerType", Material.WHITE_BANNER.name()));
         mapFillColor = config.getString("mapFillColor");
         mapLineColor = config.getString("mapLineColor");
         mapIcon = config.getString("mapIcon");
